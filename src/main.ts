f720e2949297e6db525944b78014627c8b2b59be
SilverFlin <toledorusso@outlook.com>
2023-07-26 21:38:05 -0700

    feat: google analytics gtag

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// import 'dotenv/config'
+import VueGtag from 'vue-gtag'
 
 import 'highlight.js/styles/stackoverflow-light.css'
 import 'highlight.js/lib/common';
@@ -13,8 +15,12 @@ import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 const app = createApp(App)
 
+
 app.use(createPinia())
 app.use(router)
 app.use(hljsVuePlugin)
+app.use(VueGtag, {
+    config: { id: import.meta.env.VITE_GA_MEASUREMENT_ID }
+})
 
 app.mount('#app')
