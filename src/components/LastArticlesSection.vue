97da7a038022654842b07722ce0b7f47d0c06c64
SilverFlin <toledorusso@outlook.com>
2023-07-28 17:14:52 -0700

    fix: mobile view last projects sect

--- src/components/LastArticlesSection.vue
+++ src/components/LastArticlesSection.vue
@@ -1,11 +1,17 @@
 <template>
-    <div class="flex flex-row items-center justify-center max-h-screen min-h-screen px-32 overflow-hidden bg-primary-200">
-        <article-card v-for="card in articleCardsData" :key="card.id" :card="card"></article-card>
-        <div class="hidden ml-32 md:block">
-            <router-link :to="{ name: 'articles' }">
-                <Icon width="4rem" icon="line-md:arrow-right"
-                    class="rounded-full cursor-pointer hover:text-secondary-600" />
-            </router-link>
+    <div class="max-h-screen min-h-screen overflow-hidden bg-primary-200">
+        <h1 class="my-16 text-5xl font-bold text-center uppercase lg:ml-12 text-secondary-900">
+            Last Articles
+        </h1>
+        <div class="flex flex-row items-center justify-center h-2/3">
+
+            <article-card v-for="card in articleCardsData" :key="card.id" :card="card"></article-card>
+            <div class="hidden ml-32 md:block">
+                <router-link :to="{ name: 'articles' }">
+                    <Icon width="4rem" icon="line-md:arrow-right"
+                        class="rounded-full cursor-pointer hover:text-secondary-600" />
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
--- src/components/LastProjectsSection.vue
+++ src/components/LastProjectsSection.vue
@@ -1,10 +1,16 @@
 <template>
-    <div class="flex flex-row items-center justify-center max-h-screen min-h-screen px-32 overflow-hidden bg-secondary-100">
-        <project-card v-for="card in projectCardsData" :key="card.id" :card="card"></project-card>
-        <div class="hidden ml-32 md:block">
-            <router-link :to="{ name: 'projects' }" >
-                <Icon width="4rem" icon="line-md:arrow-right" class="rounded-full cursor-pointer  hover:text-primary-300"/>
-            </router-link>
+    <div class="max-h-screen min-h-screen overflow-hidden bg-secondary-100">
+        <h1 class="my-16 text-5xl font-bold text-center uppercase lg:ml-12 text-primary-700">
+            Last Projects
+        </h1>
+        <div class="flex flex-row items-center justify-center h-2/3">
+            <project-card v-for="card in projectCardsData" :key="card.id" :card="card"></project-card>
+            <div class="hidden ml-32 md:block">
+                <router-link :to="{ name: 'projects' }">
+                    <Icon width="4rem" icon="line-md:arrow-right"
+                        class="rounded-full cursor-pointer hover:text-primary-300" />
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
